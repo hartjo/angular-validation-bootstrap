05c2950c0034c1b801f2f2391d2ed24f7e14c7c5
Jan Rainier Llarenas <rainier.llarenas@geeksnest.com>
2016-03-11 16:30:57 +0800

    push init

---
+++ angular-validation-bootstrap.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  /* global _ */
+
+  /**
+   * @ngdoc directive
+   * @name ngValidationBootstrap.directive:ngValidationBootstrap
+   * @description
+   * Validation for bootstrap framwork
+   * @author Jan Rainier Llarenas <llarenasjanrainier@gmail.com>
+   */
+  angular.module('ngValidationBootstrap', [])
+
+
+  .directive('ngvbIsempty', function() {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function(scope, element, attr, ctrl,$scope) {
+        ctrl.$viewChangeListeners.push(function() {
+          scope.$eval(attr.ngChange);
+          var valid = [{ngvbIsfor: attr.ngvbIsfor}]
+          var msgshow = [{ngvbIsmsg: attr.ngvbIsmsg}]
+          if(ctrl.$viewValue == ''){
+            scope[valid[0].ngvbIsfor] = 'has-error';
+            scope[msgshow[0].ngvbIsmsg] = true;
+          }
+          else{
+            scope[valid[0].ngvbIsfor] = '';
+            scope[msgshow[0].ngvbIsmsg] = false;
+          }
+          console.log(attr.ngvbIsmsg);
+          
+        });
+      }
+    };
+  })
+
+})();
+    
